83df78c2de7c8ca3fde6c604d82e04f4a9f3947e
unknown <Administrator@ENVIROCARE.COM>
2025-03-18 18:25:04 +0530

    Update build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html')
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+    sourcemap: true
   }
 })
