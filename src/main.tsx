80f9962b15ea706e454491ef9fc38591dd8f409a
unknown <Administrator@ENVIROCARE.COM>
2025-03-19 09:48:36 +0530

    Fix deployment configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import CRM from './CRM'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CRM />
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 )
